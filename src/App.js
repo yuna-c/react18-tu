5232056272d54069a4c8e3f8814a9557cec7c46f
tubi55 <tubi55@nate.com>
2024-01-08 15:04:49 +0900

    react18에서의 Automatic Batching 개념정리

--- src/App.js
+++ src/App.js
@@ -14,3 +14,8 @@ function App() {
 }
 
 export default App;
+/*
+  Automatic Batching
+  :여러개의 state가 하나의 핸들러함수 안쪽에서 동시에 변경이 될때 그룹으로 묶어서 한번만 렌더링 처리
+  :17에도 Batching기능이 동작되긴 하나 Promise를 반환하는 핸들러안쪽에 여러개의 state가 변경될 경우에는 동작안됨
+*/
