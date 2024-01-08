baacdf13c25e7f771296b19d0565c67906dccbda
tubi55 <tubi55@nate.com>
2024-01-08 15:38:08 +0900

    flushSync를 활용해서 특정 state값 auto batching에서 제외

--- src/App.js
+++ src/App.js
@@ -2,10 +2,37 @@ import News from './components/news/News';
 import Pics from './components/pics/Pics';
 import Visual from './components/visual/Visual';
 import './global.scss';
+import { useState } from 'react';
+import { flushSync } from 'react-dom';
 
 function App() {
+  console.log('re-render');
+  const [Count1, setCount1] = useState(1);
+  const [Count2, setCount2] = useState(2);
+  const [Count3, setCount3] = useState(3);
+
+  const returnPromise = () => {
+    return new Promise((res) => setTimeout(res, 500));
+  };
+
+  const changeState = () => {
+    returnPromise().then(() => {
+      //특정 State값을 Auto Batching에서 제외처리
+      flushSync(() => {
+        setCount1(Count1 + 1);
+      });
+      flushSync(() => {
+        setCount2(Count2 + 1);
+      });
+      setCount3(Count3 + 1);
+    });
+  };
   return (
     <div className="App">
+      <button onClick={changeState}>변경</button>
+      <h1>
+        {Count1}-{Count2}-{Count3}
+      </h1>
       <Visual />
       <News />
       <Pics />
