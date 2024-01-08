fef2ef0fe93a1ccdf77a33b8a1157a4f2d338f77
tubi55 <tubi55@nate.com>
2024-01-08 14:42:08 +0900

    Visual, News컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import News from './components/news/News';
+import Visual from './components/visual/Visual';
+import './global.scss';
+
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <Visual />
+      <News />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/news/News.jsx
@@ -0,0 +1,5 @@
+import './News.scss';
+
+export default function News() {
+  return <section className="News"></section>;
+}
